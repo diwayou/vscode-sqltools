b05d37ea80f344331877291be03df690a79775ca
diwayou <diwayou@qq.com>
2025-05-30 09:37:21 +0800

    inline source map fix vscode issue #145292

--- build-tools/common/set-defaults.js
+++ build-tools/common/set-defaults.js
@@ -37,7 +37,7 @@ module.exports = function setDefaults(config, includeDefaultPlugins = true) {
     config.optimization.minimize = false;
     config.optimization.minimizer = undefined;
   }
-  config.devtool = !IS_PRODUCTION ? 'source-map' : false;
+  config.devtool = !IS_PRODUCTION ? 'inline-source-map' : false;
   config.mode = IS_PRODUCTION ? 'production' : 'development';
   config.output = config.output || {};
   config.output.path = config.output.path || outdir;
